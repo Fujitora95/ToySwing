85dbc7a4810750a7184483a031d63ba535c6dcc3
= <=>
2020-09-14 14:28:00 +0300

    initial

---
+++ src/main/java/toyswing/Toyswing.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toyswing;
+
+/**
+ *
+ * @author Michael Kamau
+ */
+public class Toyswing {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        HomePage home = new HomePage();
+        home.setVisible(true);
+    }
+    
+    
+}
